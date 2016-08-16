e9494e05d9a596bd5137b8249f66a2757998fccd
Peter Andersson <pelleplutt1976@gmail.com>
2016-08-16 21:34:20 +0200

    moved out log dependency

--- src/com/pelleplutt/util/Log.java
+++ src/com/pelleplutt/util/Log.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-//import com.myapp.Essential; // TODO
+import com.pelleplutt.Essential;
 
 public class Log {
   public static PrintStream out = System.out;
